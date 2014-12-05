68bf9c3b24f32db480546fcd9eb0fd8eaa03036a
unknown <Elliott.CHIARADIA@SC-C333-PC09.sc.cpnv.ch>
2014-12-05 11:06:28 +0100

    Members (avec edit)

--- TCCECA/TCCECA/Members.aspx
+++ TCCECA/TCCECA/Members.aspx
@@ -2,6 +2,33 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="FeaturedContent" runat="server">
-</asp:Content>
+        <asp:DataGrid ID="Grid" runat="server" PageSize="5" AllowPaging="True" DataKeyField="UserId"
+        AutoGenerateColumns="False" CellPadding="4" BackColor="White" ForeColor="#000" GridLines="None"
+        OnEditCommand="Grid_EditCommand" OnUpdateCommand="Grid_UpdateCommand" >
+            <Columns>
+                <asp:BoundColumn HeaderText="Username" DataField="UserId" HeaderStyle-CssClass="displayNone"
+                    ItemStyle-CssClass="displayNone" FooterStyle-CssClass="displayNone">
+                </asp:BoundColumn>
+                <asp:BoundColumn HeaderText="Username" DataField="UserName">
+                </asp:BoundColumn>
+                <asp:BoundColumn HeaderText="Prénom" DataField="Firstname">
+                </asp:BoundColumn>
+                <asp:BoundColumn HeaderText="Nom" DataField="LastName">
+                </asp:BoundColumn>               
+                <asp:EditCommandColumn EditText="Edit" CancelText="Cancel" UpdateText="Update" HeaderText="Edit">
+                </asp:EditCommandColumn>
+                <asp:ButtonColumn CommandName="Delete" HeaderText="Delete" Text="Delete">
+                </asp:ButtonColumn>
+            </Columns>
+        <FooterStyle />
+        <SelectedItemStyle/>
+        <PagerStyle HorizontalAlign="Center" Mode="NumericPages" />
+        <AlternatingItemStyle />
+        <ItemStyle/>
+        <HeaderStyle BackColor="#000044" ForeColor="White" />
+        </asp:DataGrid>
+    </asp:Content>
 <asp:Content ID="Content3" ContentPlaceHolderID="MainContent" runat="server">
 </asp:Content>
+
+
--- TCCECA/TCCECA/Members.aspx.cs
+++ TCCECA/TCCECA/Members.aspx.cs
@@ -5,13 +5,64 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Collections;
+using System.Web.Security;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+
 namespace TCCECA
 {
     public partial class Members : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!Page.IsPostBack)
+            {
+                BindData();
+            }
+        }
 
+        SqlDataAdapter da;
+        DataSet ds = new DataSet();
+        SqlCommand cmd = new SqlCommand();
+        SqlConnection con;
+
+        public void BindData()
+        {
+            con = new SqlConnection(ConfigurationManager.ConnectionStrings["MembershipConnection"].ConnectionString);
+            cmd.CommandText = "Select UserId, UserName, Firstname, LastName from Users";
+            cmd.Connection = con;
+            da = new SqlDataAdapter(cmd);
+            da.Fill(ds);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            Grid.DataSource = ds;
+            Grid.DataBind();
+            con.Close();
+        }
+        protected void Grid_EditCommand(object source, DataGridCommandEventArgs e)
+        {
+            Grid.EditItemIndex = e.Item.ItemIndex;
+            BindData();
+        }
+
+        protected void Grid_UpdateCommand(object source, DataGridCommandEventArgs e)
+        {
+              SqlConnection con;
+            con = new SqlConnection(ConfigurationManager.ConnectionStrings["MembershipConnection"].ConnectionString);
+            SqlCommand cmd;
+            cmd = new SqlCommand("Update Users Set UserName = '" + ((TextBox)e.Item.Cells[1].Controls[0]).Text + "', Firstname = '" +
+                ((TextBox)e.Item.Cells[2].Controls[0]).Text + "', Lastname = '" + ((TextBox)e.Item.Cells[3].Controls[0]).Text +
+                "' where UserId='" + ((TextBox)e.Item.Cells[0].Controls[0]).Text + "';");
+            cmd.Connection = con;
+            cmd.Connection.Open();
+            cmd.ExecuteNonQuery();
+            cmd.Connection.Close();
+            Grid.EditItemIndex = -1;
+            BindData();
         }
     }
 }
